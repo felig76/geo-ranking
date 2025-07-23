8eb5fa10dffbfc8554be0f0f50de5e66358e743a
felig76 <blakyn19@gmail.com>
2025-07-23 20:11:02 -0300

    hot fix al css general

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import GamePage from "./pages/GamePage.jsx";
 import LandingPage from "./pages/LandingPage.jsx";
+import "./App.css";
 
 function App() {
   return (
