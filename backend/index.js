173fc55e5b2eb2538e4ecfa8c7ac854b1dbc2d64
felig76 <blakyn19@gmail.com>
2025-07-24 00:49:25 -0300

    cambiar direcciones de coneccion a backend

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ app.use("/api/countries", countryRoutes);
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.clear();
-    console.log("API funcionando en http://localhost:" + PORT);
+    console.log(`Conectado a MongoDB: ${conn.connection.host}`);
   });
 }).catch((err) => {
   console.error("Error al conectar a la DB:", err);
--- frontend/src/api/gameApi.jsx
+++ frontend/src/api/gameApi.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const fetchGames = async () => {
   try {
-    const response = await axios.get("http://localhost:3000/api/games");
+    const response = await axios.get("https://geo-ranking.onrender.com//api/games");
     return response.data?.data || [];
   } catch (error) {
     console.error("Error fetching games:", error);
