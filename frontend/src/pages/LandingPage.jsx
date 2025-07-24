77f4b93b90deaa30eb7aea83e5d3fd3799d80210
felig76 <blakyn19@gmail.com>
2025-07-23 21:14:07 -0300

    mejorar landing page

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,12 +1,16 @@
 import { Link } from "react-router-dom";
+import "./LandingPage.css";
 
 function LandingPage() {
-    return (
-        <div>
-            <h1>GeoRanking 🌎</h1>
-            <Link to="/play">Jugar</Link>
-        </div>
-    );
+  return (
+    <div id="landingPageContainer">
+      <div className="hero">
+        <h1 className="mainTitle">GeoRanking 🌎</h1>
+        <p className="subtitle">Test your geography skills</p>
+        <Link to="/play" className="playButton">Daily Game</Link>
+      </div>
+    </div>
+  );
 }
 
 export default LandingPage;
