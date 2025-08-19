86a653643a60bd4e1ef7539b89a69afedb67073d
felig76 <blakyn19@gmail.com>
2025-08-19 20:37:50 -0300

    hotfixes para devear

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ app.use("/api/countries", countryRoutes);
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.clear();
-    console.log(`Conectado a MongoDB: ${conn.connection.host || "localhost"}`);
+    console.log(`Conectado a MongoDB: ${process.env.MONGO_URI || " en localhost"}`);
   });
 }).catch((err) => {
   console.error("Error al conectar a la DB:", err);
